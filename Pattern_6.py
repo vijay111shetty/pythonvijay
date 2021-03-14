7dfe388c7cc032a0f2e150ca1f3b3f3a80fde574
vijay <vijay111shetty@gmail.com>
2021-03-14 08:39:48 +0530

    mypythonprog

--- Pattern_6.py
+++ Pattern_6.py
@@ -6,9 +6,17 @@ for i in range(1,6):
         print("* ",end="")
     print()
 """
+"""
 for i in range(5,0,-1):
     for j in range(i,5):
         print(" ",end="")
     for k in range(0,i):
         print("* " ,end="")
+    print()
+"""
+for i in range(5,0,-1):
+    for j in range(i,5):
+        print(" ",end="")
+    for x in range(0,i):
+        print("* ",end="")
     print()
